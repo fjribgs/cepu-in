655c2c0825860efe4c307116e9ecc8707ec4f4eb
RakanArga <rakanarga20@gmail.com>
2026-03-31 22:35:53 +0700

    [feat] : add login, register, logout (todo: fix loguot success message)

---
+++ src/app/api/auth/logout/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+import { getSession } from "../../../../../lib/auth";
+
+
+export async function POST()
+{
+    try{
+    const session = await getSession();
+    if (!session){
+        return NextResponse.json({error: "Unauthorized"}, {status: 401});
+    }
+    const cookieStore = await cookies()
+    cookieStore.delete("token")
+    
+    return NextResponse.json({message: "Berhasil log out!"}, {status: 200})
+}
+catch(error){ 
+    return NextResponse.json({message: "Error"}, {status: 500})
+}
+}
---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,44 @@
+import { NextRequest, NextResponse} from "next/server";
+import { db } from "../../../../../lib/db"; 
+import { hashPassword } from "../../../../../lib/auth";
+import { getSession } from "../../../../../lib/auth";
+
+export async function POST(req:Request) {
+    try{
+    const session = await getSession();
+    if(!session){
+        return NextResponse.json({error: "Unauthorized"}, {status: 401})
+    }
+    if(session.role !== "Admin"){
+        return NextResponse.json({error: "Forbidden"}, {status: 403})
+    }
+   const {username, nama, password} = await req.json(); 
+   const existing = await db.user.findUnique({where: {username}});
+   if(existing){
+        return NextResponse.json(
+            {error: "Username sudah ada, mohon gunakan username lain!"},
+            {status: 400}
+        )
+   }
+   const hashedPassword = await hashPassword(password);
+   
+    await db.user.create({data: {
+    username,
+    nama,
+    password: hashedPassword
+   }})
+
+   return NextResponse.json(
+    {message: "Selesai, akun sudah dibuat!"},
+    {status: 201}
+   )
+
+}
+catch(error)
+{
+ return NextResponse.json(
+    {error:"Gagal register" },
+    {status: 500}
+)
+}
+}
